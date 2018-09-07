81bf773195bc9415bf476e865e77894f36928bfa
Vipin Pandey <vipinpandey75@gmail.com>
2018-09-07 20:36:27 +0530

    Update and rename ClientUtilsApplication.java to MailApplication.java

---
+++ MailApplication.java
@@ -0,0 +1,37 @@
+package **packagename**
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Timestamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * This is the main class for MailApplication (Spring boot application).
+ */
+@SpringBootApplication
+public class MailApplication
+{
+    
+
+    String classDetails;
+
+    MailApplication()
+    {
+        classDetails = "Mail Spring boot application.";
+    }
+
+    /**
+     * Main method for Mail Application.
+     *
+     * @param args arguments to be passed to main method
+     */
+    public static void main(final String[] args)
+    {
+        SpringApplication.run(MailApplication.class, args);
+    }
+
+    
+}
