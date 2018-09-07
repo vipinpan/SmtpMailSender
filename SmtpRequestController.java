f2f69faae2a9b2817c41a603e04a71aa30d89d1b
Vipin Pandey <vipinpandey75@gmail.com>
2018-09-07 20:25:52 +0530

    Add files via upload

---
+++ SmtpRequestController.java
@@ -0,0 +1,41 @@
+package com.sophos.nsg.jtaf.clientutils.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sophos.nsg.jtaf.clientutils.utils.SmtpRequest;
+
+@RestController
+public class SmtpRequestController
+{
+
+    /**
+     * This method handles Smtp Request.
+     * It makes Smtp request from current client with given params.
+     * 
+     * @param smtpreq Object having json passed as argument to this REST API
+     * 
+     * @return response JSON having (full response and md5sum as keys.)
+     * @throws Exception.
+     * 
+     */
+    @RequestMapping(value = "/smtpreq", method = RequestMethod.POST)
+    public Map<String, String> SmtpReq(@RequestBody final SmtpRequest smtpreq)
+        throws Exception
+    {
+        Map<String, String> response = new HashMap<>();
+
+        smtpreq.makeSmtpReq();
+
+        response.put("response_message", smtpreq.getResponseMessage());
+        response.put("response_code", Integer.toString(smtpreq.getResponseCode()));
+
+        return response;
+    }
+
+}
